d5c8e839ac37e20e7d4a29b1983233f6cb901bd8
dinaldoap <dinaldoap@gmail.com>
2019-06-14 19:35:55 +0000

    feat: processa streams de arquivos

--- moa-example/src/main/java/example/InputStreamGenerator.java
+++ moa-example/src/main/java/example/InputStreamGenerator.java
@@ -1,23 +1,19 @@
 package example;
 
-import java.io.File;
 import moa.core.TimingUtils;
 import moa.options.ClassOption;
 import moa.tasks.MainTask;
 import moa.tasks.TaskThread;
 
-public class Experiment {
-
-    public Experiment() {
+public class InputStreamGenerator {
+    public InputStreamGenerator() {
     }
 
-    public void run(int numInstances, boolean isTesting) throws Exception {
-        cleanUp();
+    public void run() throws Exception {
         long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
-        String task = "EvaluatePrequential -l bayes.NaiveBayes"
-                + " -s (ArffFileStream -f data/gradual_concept_drift.arff -c -1)"
-                + " -e (FadingFactorClassificationPerformanceEvaluator)"
-                + " -i 100000 -f 1000 -d data/results.csv";
+        String task =
+                "WriteStreamToARFFFile -s (ConceptDriftStream -s (generators.SEAGenerator -f 3) -d (generators.SEAGenerator -f 2) -p 50000 -w 20000)"
+                        + " -f data/gradual_concept_drift.arff -m 100000";
         System.out.println(task);
         MainTask currentTask = (MainTask) ClassOption.cliStringToObject(task, MainTask.class, null);
         TaskThread thread = new TaskThread((moa.tasks.Task) currentTask);
@@ -27,13 +23,9 @@ public class Experiment {
         System.out.println(time + " seconds.");
     }
 
-    private void cleanUp() {
-        new File("data/results.csv").delete();
-    }
-
     public static void main(String[] args) throws Exception {
-        System.out.println("Experiment started!");
-        Experiment exp = new Experiment();
-        exp.run(1000000, true);
+        System.out.println("Generation started!");
+        InputStreamGenerator generator = new InputStreamGenerator();
+        generator.run();
     }
 }
