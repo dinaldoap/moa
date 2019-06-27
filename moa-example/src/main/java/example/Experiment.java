ca2f7c12058fea80ac04ee4fd6a4ca5cbd1686f4
dinaldoap <dinaldoap@gmail.com>
2019-06-27 21:29:23 +0000

    fix: clean previous evaluation csv

--- moa-example/src/main/java/example/Experiment.java
+++ moa-example/src/main/java/example/Experiment.java
@@ -1,6 +1,9 @@
 package example;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import moa.core.TimingUtils;
 import moa.options.ClassOption;
 import moa.tasks.MainTask;
@@ -11,13 +14,13 @@ public class Experiment {
     public void run() throws Exception {
         for (ClassifierEnum classifier : ClassifierEnum.values()) {
             for (String stream : InputStreamGenerator.STREAMS) {
-                cleanUp(stream);
+                cleanUp(classifier.getName(), stream);
                 long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
                 String task = String.format(
                         "EvaluatePrequential -l (%2$s)"
                                 + " -s (ArffFileStream -f data/stream/%1$s.arff -c -1)"
                                 + " -e (FadingFactorClassificationPerformanceEvaluator -r)"
-                                + " -i 100000 -f 1 -d data/evaluation/%3$s/%1$s.csv",
+                                + " -i 50000 -f 1 -d data/evaluation/%3$s/%1$s.csv",
                         stream, classifier.getCommand(), classifier.getName());
                 System.out.println(task);
                 MainTask currentTask =
@@ -31,9 +34,13 @@ public class Experiment {
         }
     }
 
-    private void cleanUp(String stream) {
-        new File(String.format("data/evaluation/%s.csv", stream)).delete();
+    private void cleanUp(String classifier, String stream) throws IOException {
+        Path path = Paths.get(String.format("data/evaluation/%s/%s.csv", classifier, stream));
+        if (Files.exists(path)) {
+            Files.delete(path);
+        }
     }
+
 
     public static void main(String[] args) throws Exception {
         System.out.println("Experiment started!");
